9af8152d6c210cba555e37567cfb42fd2574ae4e
Applexzs <reverseapplemw2@gmail.com>
2026-04-30 15:12:00 -0600

    added dataRestConfig to get product ID

---
+++ src/main/java/com/applexzs/springboot/backend/DataRestConfig.java
@@ -0,0 +1,16 @@
+package com.applexzs.springboot.backend;
+
+
+import com.applexzs.springboot.backend.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);
+    }
+}
