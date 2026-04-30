5ae6923c7a11c0be2fb4cc8aa5e936964445b202
Applexzs <reverseapplemw2@gmail.com>
2026-04-30 16:45:48 -0600

    Final commit: added CrossOrigin for the connection with front-end

--- src/main/java/com/applexzs/springboot/backend/repositories/IProductRepository.java
+++ src/main/java/com/applexzs/springboot/backend/repositories/IProductRepository.java
@@ -3,8 +3,9 @@ package com.applexzs.springboot.backend.repositories;
 import com.applexzs.springboot.backend.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin(originPatterns = "http://localhost:5173/")
 @RepositoryRestResource(path = "products")
 public interface IProductRepository extends CrudRepository<Product, Long> {
 }
